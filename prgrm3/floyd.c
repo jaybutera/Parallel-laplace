f106f29829d16e97516308d5223f6a6e07c4205e
Jay Butera <buterajay@gmail.com>
2017-02-27 22:32:50 -0500

    Less fucks

--- prgrm3/floyd.c
+++ prgrm3/floyd.c
@@ -14,12 +14,15 @@ void file_to_mat(char* filename, int id, int p) {
     int start_row = (id * ROWS) / p;
     int end_row   = ((id+1) * ROWS) / p - 1;
 
+    printf("Proc %d reading lines [%d-%d]\n", id, start_row, end_row);
+
     fseek(f, start_row * COLS * sizeof(double), SEEK_SET);  // Jump to the end of the file
     //long filelen = ftell(f);     // Get the current byte offset in the file
-    long filelen = (end_row - start_row) * COLS * sizeof(double);
+    long offset = (end_row - start_row + 1) * COLS * sizeof(double);
+    printf("proces %d offset: %d\n", id, offset);
     //rewind(f);              // Jump back to beginning of file
 
-    fread((void*)Astorage, filelen, 1, f);
+    fread((void*)(Astorage + start_row * COLS), offset, 1, f);
 
     fclose(f);
 }
@@ -45,29 +48,83 @@ int main (int argc, char** argv) {
         exit(0);
     }
 
-    int start_row = (id * ROWS) / p;
-    int end_row   = ((id+1) * ROWS) / p - 1;
+    int start_row = (id * ROWS) / num_procs;
+    int end_row   = ((id+1) * ROWS) / num_procs - 1;
 
     int i;
     for (i = 0; i < ROWS; i++) {
         A[i] = &Astorage[i * COLS];
     }
 
-    file_to_mat("mat");
+    file_to_mat("mat", id, num_procs);
 
+    if (id) {
+        int elem_count = (end_row-start_row)*COLS;
+        MPI_Gather( Astorage + (start_row*COLS), elem_count, MPI_DOUBLE, NULL, elem_count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    }
+    else { // Process 0
+        double* Agather = (double*) malloc(ROWS * COLS * sizeof(double));
+        if (Agather == NULL) {
+            printf("Astorage mem could not allocate\n");
+            exit(0);
+        }
+
+        double** Ag_ptr = (double**) malloc(ROWS * sizeof(double*));
+        if (Ag_ptr == NULL) {
+            printf("Astorage mem could not allocate\n");
+            exit(0);
+        }
+
+        // (start data ptr, num of elements to send,
+        int elem_count = (end_row-start_row)*COLS;
+        //MPI_Gather( Astorage + (start_row*COLS), elem_count, MPI_DOUBLE, Agather+(start_row*COLS), elem_count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather( Astorage + (start_row*COLS), elem_count, MPI_DOUBLE, Agather+(start_row*COLS), elem_count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        int j;
+        for (i = 0; i < ROWS; i++) {
+            printf("%d| ", id);
+
+            for (j = 0; j < COLS; j++)
+                printf("%f ", A[i][j]);
+            printf("\n");
+        }
+        printf("\n");
+
+        free(Ag_ptr);
+        free(Agather);
+    }
+
+    /*
     int j;
-    for (i = 0; i < start_row; i++) {
-        for (j = 0; j < end_row; j++)
+    for (i = start_row; i < end_row+1 ; i++) {
+        printf("%d| ", id);
+
+        for (j = 0; j < COLS; j++)
             printf("%f ", A[i][j]);
         printf("\n");
     }
+    printf("\n");
+    */
+
+    // If processor 0
+    /*
+    if (!id) {
+        int j;
+        for (i = 0; i < ROWS; i++) {
+            printf("%d| ", id);
+
+            for (j = 0; j < COLS; j++)
+                printf("%f ", A[i][j]);
+            printf("\n");
+        }
+        printf("\n");
+    }
+    */
 
     // Dealloc
     free(A);
     free(Astorage);
 
-
-    fflush(stdout);
     MPI_Finalize();
     return 0;
 }

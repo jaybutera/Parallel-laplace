74f33045d7308022b261f7cd394989be44862d73
Jay Butera <buterajay@gmail.com>
2017-03-01 21:55:23 -0500

    Sumtin works

--- prgrm3/floyd.c
+++ prgrm3/floyd.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <omp.h>
 
 #define ROWS 10
 #define COLS 10
 
 #define BLOCK_LOW(id,p) (id * ROWS) / p
-//#define BLOCK_OWNER(k,p) 
+#define BLOCK_OWNER(k,p,n) (k * p) / n
+#define MIN(x,y) (x > y) ? y : x
 
 double** A;
 double* Astorage;
@@ -24,41 +26,46 @@ void file_to_mat(char* filename, int id, int p) {
     fseek(f, start_row * COLS * sizeof(double), SEEK_SET);  // Jump to the end of the file
     //long filelen = ftell(f);     // Get the current byte offset in the file
     long offset = (end_row - start_row + 1) * COLS * sizeof(double);
-    printf("process %d offset: %d\n", id, offset);
-    //rewind(f);              // Jump back to beginning of file
+    //printf("process %d offset: %d\n", id, offset);
 
-    //fread((void*)(Astorage + start_row * COLS), offset, 1, f);
-    fread((void*)A[0], offset, 1, f);
+    fread((void*)(Astorage), offset, 1, f);
 
     fclose(f);
 }
 
-/*
-void compute_shortest_paths (int id, int p, double** a, int n) {
+void compute_shortest_paths (int id, int p, double** a) {
     int i, j, k;
     int offset;
     int root;
-    int* tmp;
+    double* tmp;
 
-    tmp = (double*) malloc (n * sizeof(double));
-    for (k = 0; k < n; k++) {
-        root = BLOCK_OWNER(k,p,n);
+    int start_row = (id * ROWS) / p;
+    int end_row   = ((id+1) * ROWS) / p - 1;
+    int local_rows = (end_row - start_row)+1;
+
+    tmp = (double*) malloc (COLS * sizeof(double));
+    for (k = 0; k < ROWS; k++) {
+        root = BLOCK_OWNER(k,p, ROWS);
+
+        /*
+        if (k == ROWS-1)
+            root = p-1;
+        */
 
         if (root == id) {
             offset = k - BLOCK_LOW(id,p);
-            for (j = 0; j < n; j++)
+            for (j = 0; j < ROWS; j++)
                 tmp[j] = a[offset][j];
         }
 
-        MPI_Bcast(tmp, n, MPI_DOUBLE, root, MPI_COMM_WORLD);
-        for (i = 0; i < BLOCK_SIZE(id,p,n); i++)
-            for (j = 0; j < n; j++)
-                a[i][j] = min(a[i][j], a[i][k]+tmp[j]);
+        MPI_Bcast(tmp, ROWS, MPI_DOUBLE, root, MPI_COMM_WORLD);
+        for (i = 0; i < local_rows; i++)
+            for (j = 0; j < ROWS; j++)
+                a[i][j] = MIN(a[i][j], a[i][k]+tmp[j]);
     }
 
     free( tmp );
 }
-*/
 
 int main (int argc, char** argv) {
     int id; // Process id
@@ -73,15 +80,15 @@ int main (int argc, char** argv) {
     int end_row   = ((id+1) * ROWS) / num_procs - 1;
     int local_rows = (end_row - start_row)+1;
 
-    Astorage = (double*) malloc((end_row - start_row) * COLS * sizeof(double));
+    Astorage = (double*) malloc(local_rows * COLS * sizeof(double));
     if (Astorage == NULL) {
         printf("Astorage mem could not allocate\n");
         exit(0);
     }
 
-    A = (double**) malloc(ROWS * sizeof(double*));
+    A = (double**) malloc(local_rows * sizeof(double*));
     if (A == NULL) {
-        printf("Astorage mem could not allocate\n");
+        printf("A mem could not allocate\n");
         exit(0);
     }
 
@@ -90,8 +97,10 @@ int main (int argc, char** argv) {
         A[i] = &Astorage[i * COLS];
     }
 
-    file_to_mat("mat", id, num_procs);
+    file_to_mat("mat_test", id, num_procs);
 
+
+    compute_shortest_paths(id, num_procs, A);
     /*
     int j;
     for (i = 0; i < local_rows; i++) {
@@ -103,35 +112,67 @@ int main (int argc, char** argv) {
     }
     printf("\n");
     */
+    MPI_Barrier(MPI_COMM_WORLD);
 
 
-    /*
-    if (!id) {
+    if (id == num_procs-1) {
+        // Tmp matrix
+        //----------------------------
+        double* Astorage_tmp = (double*) malloc(local_rows * COLS * sizeof(double));
+        if (Astorage_tmp == NULL) {
+            printf("Astorage mem could not allocate\n");
+            exit(0);
+        }
+
+        double** A_tmp = (double**) malloc(local_rows * sizeof(double*));
+        if (A_tmp == NULL) {
+            printf("A mem could not allocate\n");
+            exit(0);
+        }
+
+        int i;
+        for (i = 0; i < local_rows; i++) {
+            A_tmp[i] = &Astorage_tmp[i * COLS];
+        }
+        //----------------------------
+
+        MPI_Status status;
+        for (id = 0; id < num_procs-1; id++) {
+            MPI_Recv(
+                     Astorage_tmp,
+                     (ROWS / num_procs) * COLS,
+                     MPI_DOUBLE,
+                     id,
+                     0,
+                     MPI_COMM_WORLD,
+                     &status);
+
+            // Print sub matrix
+            int j;
+            for (i = 0; i < (ROWS / num_procs ); i++) {
+                for (j = 0; j < COLS; j++)
+                    printf("%f ", A_tmp[i][j]);
+                printf("\n");
+            }
+        }
+
         // Print sub matrix
         int j;
-        for (i = 0; i < ROWS; i++) {
+        for (i = 0; i < local_rows; i++) {
             for (j = 0; j < COLS; j++)
                 printf("%f ", A[i][j]);
             printf("\n");
         }
 
-        MPI_Status status;
-        for (i = 1; i < num_procs; i++) {
-            MPI_Recv(//A[ BLOCK_LOW(i, num_procs) ],
-                     &Astorage,
-                     (ROWS / num_procs + 1) * COLS,
-                     MPI_DOUBLE,
-                     i,
-                     0,
-                     MPI_COMM_WORLD,
-                     &status);
-        }
+        free(Astorage_tmp);
+        free(A_tmp);
     }
     else {
-        printf("Send from proc %d\n", id);
-        MPI_Send(&Astorage, local_rows * COLS, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        printf("Send %d elements from proc %d\n", local_rows * COLS, id);
+        MPI_Send(Astorage, local_rows * COLS, MPI_DOUBLE, num_procs-1, 0, MPI_COMM_WORLD);
     }
-    */
+
+    MPI_Barrier(MPI_COMM_WORLD);
 
     // Dealloc
     free(A);

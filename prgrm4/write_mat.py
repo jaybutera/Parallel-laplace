f90e69e1e60cfa88f50a6968468e7b6fc5c10bda
Jay Butera <buterajay@gmail.com>
2017-03-20 11:08:33 -0500

    Mat generator

---
+++ prgrm4/write_mat.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+n = 10
+
+with open('mp_mat', 'wb') as f:
+    A = np.random.rand(n,n)
+
+    # Write file as binary
+    A.astype('float32').tofile(f)
+
+    #print A
+
+'''
+with open('mat', 'rb') as f:
+    A = np.fromfile(f, dtype=np.float64)
+
+    print A
+'''

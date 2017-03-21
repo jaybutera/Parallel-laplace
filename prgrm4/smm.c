493297cda6ad6f2bc4bb53f6eaa9873f73e91de6
Jay Butera <buterajay@gmail.com>
2017-03-21 13:11:05 -0400

    Initial recursive matmul

---
+++ prgrm4/smm.c
@@ -0,0 +1,40 @@
+#define THRESHOLD 2
+
+void rec_matmul (int crow, int ccol,
+                 int arow, int acol,
+                 int brow, int bcol,
+                 int l, int m, int n,
+                 float** a, float** b, float** c)
+{
+    int lhalf[3], mhalf[3], nhalf[3];
+    int i,j,k;
+    float *aptr, *bptr, *cptr;
+
+    if (m * n > THRESHOLD) {
+        lhalf[0] = 0; lhalf[1] = l/2; lhalf[1] = l - l/2;
+        mhalf[0] = 0; mhalf[1] = m/2; mhalf[1] = m - m/2;
+        nhalf[0] = 0; nhalf[1] = n/2; nhalf[1] = n - n/2;
+
+        for (i - 0; i < 2; i++)
+            for (j = 0; j < 2; j++)
+                for (k = 0; k < 2; k++)
+                    mm( crow+lhalf[i], ccol+mhalf[j],
+                        arow+lhalf[i], acol+mhalf[k],
+                        brow+mhalf[k], bcol+nhalf[j],
+                        lhalf[i+1], mhalf[k+1], nhalf[j+1]);
+    }
+    else {
+
+        for (i = 0; i < 1; i++)
+            for (j = 0; j < n; j++) {
+                cptr = &c[crow+i][ccol+j];
+                aptr = &a[arow+i][acol];
+                bptr = &b[brow][bcol+j];
+
+                for (k = 0; k < m; k++) {
+                    *cptr += *(aptr++) * *bptr;
+                    bptr += SIZE;
+                }
+            }
+    }
+}

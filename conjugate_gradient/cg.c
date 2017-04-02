d7056faecf615ca9693111eb755963192705aa3c
Jay Butera <buterajay@gmail.com>
2017-04-02 18:37:42 -0500

    Fix residual divergence

--- conjugate_gradient/cg.c
+++ conjugate_gradient/cg.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define dtype double
+
 #define SIZE 2
 
-float inner_prod (float* a, float* b, int n) {
-    float prod;
+dtype inner_prod (dtype* a, dtype* b, int n) {
+    dtype prod = 0;
 
     int i;
     for (i = 0; i < n; i++) {
@@ -15,14 +17,14 @@ float inner_prod (float* a, float* b, int n) {
     return prod;
 }
 
-void printb (float* b, int n) {
-    int i,j;
+void printb (dtype* b, int n) {
+    int i;
     for (i = 0; i < n; i++)
         printf("%8.3f", b[i]);
     printf("\n");
 }
 
-void mat_vec_mult (float** A, float* x, float* b, int n) {
+void mat_vec_mult (dtype** A, dtype* x, dtype* b, int n) {
     int i,j;
     //printf("b\n---------\n");
     for (i = 0; i < n; i++) {
@@ -36,21 +38,21 @@ void mat_vec_mult (float** A, float* x, float* b, int n) {
     }
 }
 
-void initA (float** A, int n) {
+void initA (dtype** A, int n) {
     /*
-    float counter = 0;
+    dtype counter = 0;
     int i,j;
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
             A[i][j] = counter++;
-            */
+    */
     A[0][0] = 4;
     A[0][1] = 1;
     A[1][0] = 1;
     A[1][1] = 3;
 }
 
-void printA (float** A, int n) {
+void printA (dtype** A, int n) {
     int i,j;
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++)
@@ -60,25 +62,27 @@ void printA (float** A, int n) {
     printf("\n");
 }
 
-void initb (float* b, int n) {
+void initb (dtype* b, int n) {
     int i,j;
     for (i = 0; i < n; i++)
         b[i] = i+1;
 }
 
-void conjgrad (float** A, float* b, float* x, int n) {
+void conjgrad (dtype** A, dtype* b, dtype* x, int n) {
     int i;
     // Allocate space
-    float* residual = (float*) malloc( SIZE * sizeof(float) );
-    float* dir_vec  = (float*) malloc( SIZE * sizeof(float) );
-    float* Ap       = (float*) malloc( SIZE * sizeof(float) );
+    dtype* residual = (dtype*) malloc( SIZE * sizeof(dtype) );
+    dtype* dir_vec  = (dtype*) malloc( SIZE * sizeof(dtype) );
+    dtype* Ap       = (dtype*) malloc( SIZE * sizeof(dtype) );
+    /*
     for (i = 0; i < n; i++) {
         residual[i] = 0;
         //dir_vec[i] = 0;
         Ap[i] = 0;
     }
-    float alpha;
-    float rsnew;
+    */
+    dtype alpha;
+    dtype rsnew;
 
 
     // Compute initial residual
@@ -89,43 +93,48 @@ void conjgrad (float** A, float* b, float* x, int n) {
         // Residual is the initial search direction
         dir_vec[i] = residual[i];
     }
-    printf("residual\n----------\n");
-    printb(dir_vec,n);
+    //printf("residual\n----------\n");
+    //printb(dir_vec,n);
 
-    float rsold = inner_prod(residual, residual, n);
+    dtype rsold = inner_prod(residual, residual, n);
 
     int j;
+    //printf("residuals\n------------\n");
     for (i = 0; i < n; i++) {
         mat_vec_mult(A, dir_vec, Ap, n);
-        printf("Ap\n-----------\n");
-        printb(Ap, n);
+        //printf("Ap\n-----------\n");
+        //printb(Ap, n);
 
         //printf("---\np * Ap: %f\n", inner_prod(dir_vec, Ap, n));
 
         alpha = rsold / inner_prod(dir_vec, Ap, n);
-        printf("\nalpha: %f\n", alpha);
+        //printf("\nalpha: %f\n", alpha);
 
         // Update x
         for (j = 0; j < n; j++)
             x[j] += alpha * dir_vec[j];
-        printf("\nnew x:\n");
-        printb(x,n);
+        //printf("\nnew x:\n");
+        //printb(x,n);
 
         // Update residual
         for (j = 0; j < n; j++)
             residual[j] -= alpha * Ap[j];
-        printf("residual\n----------\n");
-        printb(residual,n);
+        //printf("residual\n----------\n");
+        //printb(residual,n);
 
         rsnew = inner_prod(residual, residual, n);
+        printf("%f\n", sqrt(rsnew));
 
-        if (sqrt(rsnew) < .001) break;
+        if (sqrt(rsnew) < .000001) {
+            printf("MINIMAL ERROR, EXIT\n");
+            break;
+        }
 
         // Update direction vector
         for (j = 0; j < n; j++)
             dir_vec[j] = residual[j] + (rsnew/rsold) * dir_vec[j];
-        printf("\ndir vec\n----------\n");
-        printb(dir_vec,n);
+        //printf("\ndir vec\n----------\n");
+        //printb(dir_vec,n);
 
         rsold = rsnew;
     }
@@ -140,13 +149,13 @@ int main(int argc, char** argv) {
 
     // Init matrix A
     // -------------
-    float* Astorage = malloc ( SIZE * SIZE * sizeof(float) );
+    dtype* Astorage = malloc ( SIZE * SIZE * sizeof(dtype) );
     if (Astorage == NULL) {
         printf("Astorage mem could not allocate\n");
         exit(0);
     }
 
-    float** A = (float**) malloc(SIZE * sizeof(float*));
+    dtype** A = (dtype**) malloc(SIZE * sizeof(dtype*));
     if (A == NULL) {
         printf("A mem could not allocate\n");
         exit(0);
@@ -162,7 +171,7 @@ int main(int argc, char** argv) {
 
     // Init vector b
     // -------------
-    float* b = (float*) malloc(SIZE * sizeof(float*));
+    dtype* b = (dtype*) malloc(SIZE * sizeof(dtype*));
     if (b == NULL) {
         printf("A mem could not allocate\n");
         exit(0);
@@ -174,15 +183,13 @@ int main(int argc, char** argv) {
 
     // Init vector x
     // -------------
-    float* x = (float*) malloc (SIZE * sizeof(float));
+    dtype* x = (dtype*) malloc (SIZE * sizeof(dtype));
     if (x == NULL) {
         printf("x mem could not allocate\n");
         exit(0);
     }
-    /*
     for (i = 0; i < SIZE; i++)
         x[i] = 0;
-        */
     x[0] = 2;
     x[1] = 1;
     // -------------
